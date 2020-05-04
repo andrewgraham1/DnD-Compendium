ba0d9e88e6d5b4241f7b48d14b8f86197512bebf
Andrew Graham <andrewgraham1@hotmail.co.uk>
2020-05-04 11:34:00 +0100

    to do: fix thumbnail scalling

--- src/components/EncyclopediaHome/EncyclopediaHome.tsx
+++ src/components/EncyclopediaHome/EncyclopediaHome.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
 
 import "./EncyclopediaHome.css";
-import SignUpForm from "../SignUpForm";
+import KingdomsThumbnail from "./../../images/Kingdoms_thumbnail.jpg";
 
 const EncyclopediaHome = () => {
   return (
     <div className="encyclopedianContainer">
-      <div className="Kingdoms" />
+      <div className="Kingdoms">
+        <img
+          className="KingdomThumbnail"
+          src={KingdomsThumbnail}
+          alt="Thumbnail of the kingdoms button"
+        />
+      </div>
       <div className="Settlements" />
       <div className="Characters" />
       <div className="Items" />
